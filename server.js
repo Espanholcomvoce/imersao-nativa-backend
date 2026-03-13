9f0ee23ce1afefb7be1a0d35362cbe0bc29ec04b
Espanholcomvoce <espanholcvfaixapreta@gmail.com>
2026-03-13 15:54:46 -0300

    Update server.js

--- server.js
+++ server.js
@@ -42,7 +42,8 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:5500',
   'http://127.0.0.1:5500',
-  'http://localhost:8080'
+  'http://localhost:8080',
+  'https://app.espanholcomvoce.com',
 ].filter(Boolean);
 
 app.use(cors({
